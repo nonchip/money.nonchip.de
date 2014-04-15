d916827777f0032ca0f02d7b33108e59e005506b
Michael 'nonchip' Zimmer <zimmemic25@yahoo.de>
2014-04-15 11:01:20 +0200

    hosting local copies of all css libs and styles so https doesn't annoy me anymore

--- app/templates/global.php
+++ app/templates/global.php
@@ -4,25 +4,25 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=(strlen($header)>0?$header.' :: ':'')?>money.nonchip.de</title>
-    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure-min.css">
+    <link rel="stylesheet" href="/static/pure-min.css">
     <!--[if lte IE 8]>
     <link rel="stylesheet"
-          href="//purecss.io/combo/1.11.5?/css/main-grid-old-ie.css&/css/main-old-ie.css&/css/customize.css&/css/rainbow/baby-blue.css">
+          href="/static/pure-ie.css">
     <![endif]-->
     <!--[if gt IE 8]><!-->
     <link rel="stylesheet"
-          href="//purecss.io/combo/1.11.5?/css/main-grid.css&/css/main.css&/css/customize.css&/css/rainbow/baby-blue.css">
+          href="/static/pure-style.css">
     <!--<![endif]-->
     <!--[if lt IE 9]>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
+    <script src="/static/html5shiv.js"></script>
     <![endif]-->
 
     <link rel="stylesheet/less" type="text/css" href="/static/hope.less" />
-    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.0/less.min.js"></script>
+    <script src="/static/less.min.js"></script>
 
 </head>
 <body>
-<a href="http://github.com/nonchip/money.nonchip.de" target="_blank" style="z-index:999"><img style="z-index:999;position: absolute; top: 0; right: 0; border: 0;" src="http://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
+<a href="//github.com/nonchip/money.nonchip.de" target="_blank" style="z-index:999"><img style="z-index:999;position: absolute; top: 0; right: 0; border: 0;" src="/static/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
 <div id="layout">
 <a href="#menu" id="menuLink" class="menu-link">
     <span></span>
@@ -55,7 +55,7 @@
 </div>
 
 
-<script src="//purecss.io/combo/1.11.5?/js/ui.js&/vendor/rainbow/js/rainbow.min.js&/vendor/rainbow/js/language/generic.js&/vendor/rainbow/js/language/html.js&/vendor/rainbow/js/language/css.js&/vendor/rainbow/js/language/javascript.js&/vendor/rainbow/js/language/shell.js"></script>
+<script src="/static/pure-script.js"></script>
 
 
 </body>
